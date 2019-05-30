7d4553701f9e0802cc7f99f79fa8c8e91b642a29
Alexander Baltsevich <alexanderbaltsevich@MacBook-Pro-Alexander.local>
2019-05-31 01:19:42 +0300

    Updates

---
+++ precache-manifest.1d8d795fbe48e260ed5d79ab888a9bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7bdd6241d1679729d22425c02b6917",
+    "url": "/blockchain-shop/index.html"
+  },
+  {
+    "revision": "41051d0a0cdf16adf906",
+    "url": "/blockchain-shop/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "67979079ab845ee58120",
+    "url": "/blockchain-shop/static/js/2.a7e8d7fb.chunk.js"
+  },
+  {
+    "revision": "41051d0a0cdf16adf906",
+    "url": "/blockchain-shop/static/js/main.516eec4b.chunk.js"
+  },
+  {
+    "revision": "b37bd1de32c84a4717d1",
+    "url": "/blockchain-shop/static/js/runtime~main.83319e37.js"
+  },
+  {
+    "revision": "ba25863b802d9f419b62f9fabda1a345",
+    "url": "/blockchain-shop/static/media/no-image-available.ba25863b.png"
+  }
+]);
